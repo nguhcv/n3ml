2b86aed3e3378ea39250a216ed8eec264f2ae2e0
nguhcv <46445099+nguhcv@users.noreply.github.com>
2021-01-04 17:14:29 -0800

    Add files via upload

---
+++ Spikeprob_simulator/Learning.py
@@ -0,0 +1,17 @@
+class SpikeProp:
+    def __init__(self, lr):
+        self.name = 'Spikeprob'
+        self.lr = lr
+        self.terminals = 16
+
+        pass
+
+
+class STDP:
+    def __init__(self):
+        pass
+
+
+class Competitive_NormAD:
+    def __init__(self):
+        pass
